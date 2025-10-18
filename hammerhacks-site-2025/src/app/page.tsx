ee9612cc3f542027ff8f895fea66e3e27c9a654a
gbtsui <gabriel.tsui@protonmail.com>
2025-10-18 17:21:01 -0400

    feat: began migration to next, componentized header

---
+++ hammerhacks-site-2025/src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import Header from "@/components/header";
+import AboutUs from "@/components/about-us";
+
+export default function HomePage() {
+  return (
+    <div className="caps">
+        <Header/>
+    </div>
+  )
+}
